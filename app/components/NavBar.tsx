2317ed75df071532cdc98c83151c257fdb78d8fa
Vinicius Lidington <vinicius.lins@encora.com>
2026-02-11 16:33:23 -0300

    navbar with mui

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box
+          component={Link}
+          href="/"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            textDecoration: 'none',
+            color: 'inherit',
+            flexGrow: 1,
+          }}
+        >
+          <Box sx={{mr:2, display:'flex'}}>
+            <Image src="/icons/icons8-rick-sanchez.svg" alt="Logo" width={40} height={40} />
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ fontWeight: 'bold'}}
+          >
+            Rick and Morty Characterverse
+          </Typography>
+        </Box>
+        <Box sx={{ display:'flex'}}>
+          <Button color="inherit" component={Link} href="/">Home</Button>
+          <Button color="inherit" component={Link} href="/characters">Characters</Button>
+          <Button color="inherit" component={Link} href="/episodes">Episodes</Button>
+          <Button color="inherit" component={Link} href="/locations">Locations</Button>
+        </Box>
+        
+
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar
